392982fe983476551184bf4d3342511f75dfedd5
joaovictorino <jhvictorino@gmail.com>
2022-07-08 04:00:20 +0000

    create connection

--- API/index.js
+++ API/index.js
@@ -7,39 +7,44 @@ app.use(bodyParser.json())
 const port = 80
 const opt = { credentials: require('amqplib').credentials.plain('joao', 'Teste@admin123')  }
 
-app.post('/', (req, res) => {
-    if (process.env.LOCAL){
-        amqp.connect(process.env.QUEUE, connect);
-    }else{
-        amqp.connect(process.env.QUEUE, opt, connect);
-    }   
-        
-    function connect (err, conn) {
-        conn.createChannel((err, ch) =>{
-            ch.assertQueue('', {exclusive:true}, (err, q) =>{
-                const correlation = uuid.v1();
+if (process.env.LOCAL){
+    amqp.connect(process.env.QUEUE, connect);
+}else{
+    amqp.connect(process.env.QUEUE, opt, connect);
+}
 
-                console.log(`Sending message "${req.body.partner}" to queue`)
+let channel, queue_receive;
 
-                var queue = 'rpc_queue';
-
-                if(req.body.partner == 'partner1'){
-                    queue = 'rpc_queue_partner1';
-                } 
-
-                ch.sendToQueue(queue, 
-                            Buffer.from(`{ "partner" : "${req.body.partner}" }`),
-                            {correlationId:correlation, replyTo: q.queue});
-
-                ch.consume(q.queue, (msg) => {
-                    console.log(`Received from queue ${msg.content}`);
-                    if(msg.properties.correlationId == correlation){
-                        res.send(msg.content.toString());
-                    }
-                }, {noAck: true});
-            });
+function connect (err, conn) {
+    conn.createChannel((err, ch) =>{
+        channel = ch;
+        ch.assertQueue('', {exclusive:true}, (err, q) =>{
+            queue_receive = q;
         });
-    }
+    });
+}
+
+app.post('/', (req, res) => {
+    const correlation = uuid.v1();
+
+    console.log(`Sending message "${req.body.partner}" to queue`)
+
+    var queue = 'rpc_queue';
+
+    if(req.body.partner == 'partner1'){
+        queue = 'rpc_queue_partner1';
+    } 
+
+    channel.sendToQueue(queue, 
+                Buffer.from(`{ "partner" : "${req.body.partner}" }`),
+                {correlationId:correlation, replyTo: queue_receive.queue});
+
+    channel.consume(queue_receive.queue, (msg) => {
+        console.log(`Received from queue ${msg.content}`);
+        if(msg.properties.correlationId == correlation){
+            res.send(msg.content.toString());
+        }
+    }, {noAck: true});
 })
 
 app.get('/', (req, res) =>{
